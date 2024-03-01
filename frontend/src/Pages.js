5776da4a7f92c6dde1eff94eb3ac4c42ef06c315
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-03-01 18:57:22 +1100

    set up pages layout

---
+++ frontend/src/Pages.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './screens/Home';
+
+const Pages = () => {
+  return (
+    <>
+      <Routes>
+				<Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
+
+export default Pages
