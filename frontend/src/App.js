97e27e541f125ba2aed1954ff5608ef3c6dd1806
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-03-01 19:19:27 +1100

    set up tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 import Pages from './Pages'
 import { Box } from '@mui/material';
 
+import 'tailwindcss/tailwind.css';
+
 function App() {
   return (
     <BrowserRouter>
--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>This is Home</div>
+    <div className="text-6xl">This is Home</div>
   )
 }
 
