69a13f978ab8af6de64aaa28301fec9d4a94e5ee
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-03-01 22:36:20 +1100

    created sign in and register page

--- frontend/src/Pages.js
+++ frontend/src/Pages.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './screens/Home';
+import Login from './screens/Login';
+import Register from './screens/Register';
 
 const Pages = () => {
   return (
     <>
       <Routes>
 				<Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
